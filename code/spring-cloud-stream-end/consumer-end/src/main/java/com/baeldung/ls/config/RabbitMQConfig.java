2834612f4af2d7868761277ef0cbb82774e8d442
Fede <federico.canali00@gmail.com>
2024-06-15 20:01:13 +0200

    Added configuration for dead letter queue with dl microservice that reads from the exchange dl

--- code/spring-cloud-stream-end/consumer-end/src/main/java/com/baeldung/ls/config/RabbitMQConfig.java
+++ code/spring-cloud-stream-end/consumer-end/src/main/java/com/baeldung/ls/config/RabbitMQConfig.java
@@ -7,8 +7,10 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("auditgroup")
 public class RabbitMQConfig {
 
     @Value("${spring.cloud.stream.rabbit.bindings.messageProcessor-in-0.consumer.dead-letter-exchange}")
