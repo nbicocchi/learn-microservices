5fc8229a83a1b6acf20f8ad66a806fc680a81ae7
Davide Palma (ElDavo) <github@davidepalma.it>
2024-07-03 12:48:45 +0200

    resiliency: date: add rate-limit
    Adds a rate limiter of 20 requests per minute to the date microservice

--- code/spring-cloud-resiliency-end/date-service-end/src/main/java/com/baeldung/date/DateController.java
+++ code/spring-cloud-resiliency-end/date-service-end/src/main/java/com/baeldung/date/DateController.java
@@ -1,5 +1,6 @@
 package com.baeldung.date;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class DateController {
     private static final RandomGenerator RND = RandomGenerator.getDefault();
 
     @GetMapping(value = "/date")
+    @RateLimiter(name = "dateRateLimiter")
     public Mono<LocalDate> date(
             @RequestParam(value = "delay", required = false, defaultValue = "0") int delay
     ) {
