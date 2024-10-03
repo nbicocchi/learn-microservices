41091f72b221b90d8cbbe0d821c526b48e462339
Nicola Bicocchi <nbicocchi@unimore.it>
2024-10-04 00:27:21 +0200

    update

---
+++ modules/shared-code/product-service-no-db/src/main/java/com/baeldung/product/persistence/model/Product.java
@@ -0,0 +1,20 @@
+package com.baeldung.product.persistence.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    private Long id;
+    private String name;
+    private Double weight;
+
+    public Product(String name, Double weight) {
+        this(null, name, weight);
+    }
+
+    public Product(Product product) {
+        this(product.getId(), product.getName(), product.getWeight());
+    }
+}
---
+++ modules/shared-code/product-service-no-db/src/test/java/com/baeldung/product/ProductServiceAppTest.java
@@ -0,0 +1,11 @@
+package com.baeldung.product;
+
+import org.junit.jupiter.api.Test;
+
+class ProductServiceAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
