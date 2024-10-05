2e4896b498ce80b38ee982ebd431abf8d4bc883d
Nicola Bicocchi <nbicocchi@unimore.it>
2024-10-05 18:43:09 +0200

    update

--- modules/shared-code/product-service-postgres/product-service-postgres/src/main/java/com/baeldung/product/persistence/model/Product.java
+++ modules/shared-code/product-service-postgres/product-service-postgres/src/main/java/com/baeldung/product/persistence/model/Product.java
@@ -1,11 +1,18 @@
 package com.baeldung.product.persistence.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double weight;
