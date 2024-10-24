fd4aeb7f50d7b4a1478511d53fe101b66aa702cd
Nicola Bicocchi <nbicocchi@unimore.it>
2024-10-25 00:33:47 +0200

    update

--- modules/software-architectures/code/modular-monolith/src/main/java/com/nbicocchi/monolith/App.java
+++ modules/software-architectures/code/modular-monolith/src/main/java/com/nbicocchi/monolith/App.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ModularMonolithApp {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(ModularMonolithApp.class, args);
+        SpringApplication.run(App.class, args);
     }
-
 }
