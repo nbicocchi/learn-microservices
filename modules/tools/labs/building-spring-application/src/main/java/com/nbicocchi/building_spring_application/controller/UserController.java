c3998195f81600ae1acd76de45782810c1d06cb5
Nicola Bicocchi <nbicocchi@unimore.it>
2025-03-03 13:26:09 +0100

    update

--- modules/tools/labs/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/controller/UserController.java
+++ modules/tools/labs/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/controller/UserController.java
@@ -3,37 +3,73 @@ package com.nbicocchi.building_spring_application.controller;
 import com.nbicocchi.building_spring_application.model.UserModel;
 import com.nbicocchi.building_spring_application.service.UserService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Log4j2
 @RestController
-@RequestMapping("/users")
 public class UserController {
     UserService userService;
+    String appName;
+    String appVersion;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService,
+                          @Value("${app.name}") String appName,
+                          @Value("${app.version}") String appVersion) {
         this.userService = userService;
+        this.appName = appName;
+        this.appVersion = appVersion;
     }
 
-    @GetMapping("/{email}")
-    public UserModel getUser(@PathVariable String email) {
-        log.info("getUser() invoked");
+    @GetMapping("/greet")
+    public Map<String, String> greet() {
+        log.debug("greet() invoked");
+        return Map.of(
+                "Application name", appName,
+                "Application version", appVersion);
+    }
+
+    @GetMapping("/sleep")
+    public void sleep() throws InterruptedException {
+        log.debug("sleep() invoked");
+        Thread.sleep(100);
+    }
+
+    @GetMapping("/users")
+    public Iterable<UserModel> findAll() {
+        log.debug("findAll() invoked");
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/{email}")
+    public UserModel findUserModelByEmail(@PathVariable String email) {
+        log.debug("findUserModelByEmail() invoked");
         return userService.findUserModelByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping
-    public UserModel createUser(@RequestBody UserModel userModel) {
-        log.info("createUser() invoked");
+    @PostMapping("/users")
+    public UserModel save(@RequestBody UserModel userModel) {
+        log.debug("save() invoked");
         return userService.save(userModel);
     }
 
-    @DeleteMapping("/{email}")
-    public void deleteUser(@PathVariable String email) {
-        log.info("deleteUser() invoked");
+    @PutMapping("/users/{email}")
+    public void update(@PathVariable String email, @RequestBody UserModel userModel) {
+        log.debug("update() invoked");
+        Optional<UserModel> optionalUser = userService.findUserModelByEmail(email);
+        optionalUser.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        userModel.setId(optionalUser.get().getId());
+        userService.save(userModel);
+    }
+
+    @DeleteMapping("/users/{email}")
+    public void delete(@PathVariable String email) {
+        log.debug("delete() invoked");
         Optional<UserModel> optionalUser = userService.findUserModelByEmail(email);
         optionalUser.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         userService.delete(optionalUser.get());
--- modules/tools/labs/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/model/UserModel.java
+++ modules/tools/labs/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/model/UserModel.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Data
 @Entity
 public class UserModel {
@@ -13,6 +16,7 @@ public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
+    @NonNull private String email;
+    @NonNull private String name;
+    @NonNull private String country;
 }
