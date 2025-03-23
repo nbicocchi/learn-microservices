5e7d47158c99023456207a762c2534f896dee98f
Nicola Bicocchi <nbicocchi@unimore.it>
2025-03-24 00:36:47 +0100

    update

--- modules/infrastructure/labs/service-discovery-eureka/datetime-composite-service/src/main/java/com/nbicocchi/composite/model/LocalDateTimeWithTimestamp.java
+++
@@ -1,9 +0,0 @@
-package com.nbicocchi.composite.model;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-public record LocalDateTimeWithTimestamp(
-        LocalDate localDate, LocalTime localTime, LocalDateTime timestamp) {
-}
