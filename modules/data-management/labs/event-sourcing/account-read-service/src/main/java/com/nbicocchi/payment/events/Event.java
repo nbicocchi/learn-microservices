cc965c0097c213dad5b62da1883d37e9971690a6
Nicola Bicocchi <nbicocchi@unimore.it>
2025-11-18 13:41:29 +0100

    update

---
+++ modules/data-management/labs/event-sourcing/account-read-service/src/main/java/com/nbicocchi/payment/events/Event.java
@@ -0,0 +1,15 @@
+package com.nbicocchi.payment.events;
+
+import lombok.*;
+
+import java.time.ZonedDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Event<K, T> {
+    @NonNull private K key;
+    @NonNull private T data;
+    private ZonedDateTime eventCreatedAt = ZonedDateTime.now();
+}
---
+++ modules/data-management/labs/event-sourcing/account-read-service/src/main/java/com/nbicocchi/payment/events/MoneyDeposited.java
@@ -0,0 +1,9 @@
+package com.nbicocchi.payment.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MoneyDeposited extends AccountEvent {
+}
