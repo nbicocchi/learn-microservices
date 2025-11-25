4378af5af900371e379ac3e1b53e9b64666d0dd1
Nicola Bicocchi <nbicocchi@unimore.it>
2025-11-25 11:57:30 +0100

    update

--- modules_extra/reactive/code/spring-with-reactor-end/spring-with-reactor-postgres-end/src/main/java/com/baeldung/lsd/persistence/repository/IProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.lsd.persistence.repository;
-
-import com.baeldung.lsd.persistence.model.Project;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IProjectRepository extends CrudRepository<Project, String> {
-    Project findByCode(String code);
-
-}
