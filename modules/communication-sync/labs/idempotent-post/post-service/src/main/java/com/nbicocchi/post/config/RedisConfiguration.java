444e4d251da0afeea46b8f9d9cc59388aeeb2501
Nicola Bicocchi <nbicocchi@unimore.it>
2026-03-09 14:44:43 +0100

    update

--- modules/communication-sync/labs/idempotent-post/post-service/src/main/java/com/nbicocchi/post/config/RedisConfiguration.java
+++ modules/communication-sync/labs/idempotent-post/post-service/src/main/java/com/nbicocchi/post/config/RedisConfiguration.java
@@ -3,20 +3,14 @@ package com.nbicocchi.post.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
 
 @Configuration
 class RedisConfiguration {
-    @Bean
-    LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
 
     @Bean
     StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
+        return new StringRedisTemplate(redisConnectionFactory);
     }
 }
